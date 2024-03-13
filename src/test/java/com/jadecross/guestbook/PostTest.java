95e8fe7050bc3268cb50ccda2170955fcd0d0926
lucykim1 <ksb12589@gmail.com>
2024-03-13 16:36:09 +0900

    test

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -1,7 +1,7 @@
 ‌package com.jadecross.guestbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import  org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 class PostTest {
     @Test
